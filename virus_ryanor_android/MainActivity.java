6c354c18603ef24d4781d372d55cde586d30dd12
Ryanor <gwuehrer@gmx.at>
2016-03-10 16:50:36 +0100

    New method

--- virus_ryanor_android/MainActivity.java
+++ virus_ryanor_android/MainActivity.java
@@ -1,13 +1,25 @@
 class MainActivity {
- public void main() {
-	System.out.println("HelloWorld");
-}
+	public void main() {
+	  System.out.println("HelloWorld");
+
+	  if(this ){
+	    doNothing();	
+	  } else {
+	    doSomething();
+	  }
+	}
 
 
-public void thisIsANewMethod() { 
-	System.out.println("HelloWolrd");
+	public void thisIsANewMethod() { 
+	  
+	  System.out.println("HelloWolrd");
 
-System.out.println("Hello again");
-}
+	  System.out.println("Hello again");
+	}
+
+	public void thisIsAnotherMethod() {
+
+	System.outp.println("System down");
+	} 
 
 }
